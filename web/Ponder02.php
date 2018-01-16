d513947bff5d2e96b500db5537a944c9a6a551f8
Michael <mrmichaelgraver@gmail.com>
2018-01-16 14:40:53 -0700

    Finding mistake

--- web/Ponder02.php
+++ web/Ponder02.php
@@ -17,7 +17,7 @@
 	 			print("<p> I am glad you liked my page.  Any comments you have left for me will be taken into consideration
 	 						for further imporvement.  I hope that you visit the page again in the future.  Thank you! </p>");
 	 		} else {
-	 			print("<p> I am sorry that you did not like my page.  I will take your comments into carful consideration
+	 			print("<p> $response I am sorry that you did not like my page.  I will take your comments into carful consideration
 	 				       when revamping the site.  Visit again in the future to see your suggestions implemented.</p>");
 	 		}
 	 	?>
