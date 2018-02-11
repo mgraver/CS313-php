111f3600853d56097e188f48954850d2ec6d7d83
Michael <mrmichaelgraver@gmail.com>
2018-02-10 21:34:10 -0700

    fixed one last re-direct

--- web/Project01/logout.php
+++ web/Project01/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 session_unset();
-$URL = "http://localhost/project01/home.php";
+$URL = "https://afternoon-coast-14408.herokuapp.com/Project01/home.php";
 header("Location: " . $URL);
 exit();	
 ?>
